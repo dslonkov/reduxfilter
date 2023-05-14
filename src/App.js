049874fba9cb1635f74514b29897cc41f47d6b1e
Dmitriy Slonkov <slonkov.d@citilink.ru>
2023-05-14 23:16:08 +0300

    http1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import {addPost, deletePost} from "./redux/reducers/posts";
 
 function App() {
+
+  const dispatch = useDispatch();
+  const posts = useSelector(store => store.posts.posts);
+
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" className="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      <input type="text" className="price" value={price} onChange={(e) => setPrice(e.target.value)}/>
+      <button type='button' onClick={() => {
+        dispatch(addPost(title, price));
+        setTitle('')
+        setPrice('');
+      }}>Save</button>
+      <button>Cancel</button>
+
+      <ul className="list">
+
+        {
+          posts.map((item) => (
+            <li key={item.id}>
+              <span>
+                {item.title}
+              </span>
+
+              <span>
+                {item.price}
+              </span>
+
+              <button onClick={() => dispatch(deletePost(item.id))}>Х</button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
---
+++ src/redux/reducers/posts.js
@@ -0,0 +1,49 @@
+const ADD = 'ADD';
+const DELETE = 'DELETE';
+
+const initialState = {
+  posts: [],
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD : {
+      return {
+        ...state,
+        posts: [...state.posts, {
+          title: action.title,
+          price: action.price,
+          id: Math.floor(Math.random() * 100000)
+        }]
+      }
+    }
+
+    case DELETE : {
+      return {
+        ...state,
+          posts: state.posts.filter((item) => item.id !== action.id)
+      }
+    }
+
+    default: return state;
+  }
+}
+
+export const addPost = (title, price) => {
+  return (dispatch) => {
+      return dispatch({
+        type: ADD,
+        title,
+        price,
+      })
+  }
+}
+
+export const deletePost = (id) => {
+  return (dispatch) => {
+    return dispatch({
+      type: DELETE,
+      id,
+    })
+  }
+}
